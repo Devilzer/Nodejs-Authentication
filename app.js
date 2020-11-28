1412c89ce1966039ac5170f11b2b0b2d34e311a8
Devilzer <jenadeepak724@gmail.com>
2020-11-28 23:46:07 +0530

    add signin page ui

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const path = require("path");
+const sassMiddleware = require("node-sass-middleware");
 
+//scss middleware
+app.use(sassMiddleware({
+    src:"./assets/scss/",
+    dest:"./assets/css/",
+    debug:true,
+    outputStyle:'extended',
+    prefix :"/css"
+}));
+app.use(express.urlencoded());
+
+//setting up views
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views/"));
+app.use(express.static("assets/"));
 //using routes..
 app.use("/",require("./routes/index"));
 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/",(req,res)=>{
- return res.render();
-
-
+router.get("/signin",(req,res)=>{
+ return res.render("signin");
 });
 
 module.exports = router;
